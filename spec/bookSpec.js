67b1b17df1d3bcbea68296157fc394e8d9a38459
Nadjib Amar <hello@ebabel.eu>
2013-06-10 10:39:04 +0200

    UPDATED added a new test and reformulated all the tests to use should, to be more like BDD tests.

--- spec/bookSpec.js
+++ spec/bookSpec.js
@@ -1,22 +1,22 @@
 describe( "Namespace", function () {
 
-    it( "exists.", function () {
+    it( "should exist.", function () {
         expect( namespace ).toBeDefined();
     } );
 
-    it( "is not its initial default value, i.e. an empty object.", function () {
+    it( "should no longer be its initial default value.", function () {
         expect( namespace ).toNotBe( {} );
     } );
 
-    it( "contains a \"book\" member of type \"function\".", function () {
+    it( "should contain a \"book\" member of type \"function\".", function () {
         expect( typeof( namespace.book ) ).toBe( "function" );
     } );
 
-    it( "contains a \"query\" member of type \"function\".", function () {
+    it( "should contain a \"query\" member of type \"function\".", function () {
         expect( typeof( namespace.query ) ).toBe( "function" );
     } );
 
-    it( "contains a \"data\" member of type \"object\".", function () {
+    it( "should contain a \"data\" member of type \"object\".", function () {
         expect( typeof( namespace.data ) ).toBe( "object" );
     } );
 
@@ -24,15 +24,15 @@ describe( "Namespace", function () {
 
 describe( "Book function", function () {
 
-    it( "returns an object with the expected title \"A Practitioner's Guide to Software Test Design\".", function () {
+    it( "should returns an object with the expected title \"A Practitioner's Guide to Software Test Design\".", function () {
         expect( namespace.book( { title: "A Practitioner's Guide to Software Test Design" } ).title ).toBe( "A Practitioner's Guide to Software Test Design" );
     } );
 
-    it( "does not return any object when it isn't supplied with any parameter.", function () {
+    it( "should not return any object when it isn't supplied with any parameter.", function () {
         expect( namespace.book( ) ).toBe( undefined );
     } );
 
-    it( "does not return any object when it isn't supplied with at least one valid parameter.", function () {
+    it( "should not return any object if it isn't supplied with at least one valid parameter.", function () {
         expect( namespace.book( {} ) ).toBe( undefined );
     } )
 
@@ -40,24 +40,24 @@ describe( "Book function", function () {
 
 describe( "Data query", function () {
 
-    it( "can find at least one book by the title \"Les Hauts de Hurle-Vent\".", function () {
+    it( "should find at least one book by the title \"Les Hauts de Hurle-Vent\".", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Les Hauts de Hurle-Vent" } ).results.length ).toBeGreaterThan( 0 );
     } );
 
-    it( "can return something for an existing book found by its title.", function () {
+    it( "should return something for an existing book found by its title.", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Wuthering Heights" } ).first() ).toBeTruthy();
     } );
 
-    it( "can read title of the returned book first occurence.", function () {
-        expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Wuthering Heights" } ).first().title ).toBe( "Wuthering Heights" );
+    it( "should return the expected title \"Wuthering Heights\" for a book found by the ISBN 10 \"1853260010\".", function () {
+        expect(namespace.query( { collection: namespace.data.books, filterBy: "isbn10", filteredValue: "1853260010" } ).first().title ).toBe( "Wuthering Heights" );
     } );
 
-    it( "does not find a book that doesn't exist.", function () {
+    it( "should not find a book that doesn't exist.", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Book title that will not be found" } ).first() ).toBe( undefined );
     } );
 
-    it( "finds a book and reads its title by filtering with its ISBN 10.", function () {
-        expect( namespace.query( { collection: namespace.data.books, filterBy: "isbn10", filteredValue: "1853260010" } ).first().title ).toBe( "Wuthering Heights" );
+    it( "should return a results with 0 members when looking for a book that doesn't exist.", function () {
+        expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Book title that will not be found" } ).results.length ).toBe( 0 );
     } );
 
 } );
