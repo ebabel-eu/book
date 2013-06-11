677a6d3f936412c4cf20f7fcf6be48b38db40842
Nadjib Amar <hello@ebabel.eu>
2013-06-11 09:16:21 +0200

    UPDATED switched from logging errors to using error objects from the namespace data.

--- spec/bookSpec.js
+++ spec/bookSpec.js
@@ -1,22 +1,22 @@
 describe( "Namespace", function () {
 
-    it( "exists.", function () {
-        expect( namespace ).toBeTruthy();
+    it( "should exist.", function () {
+        expect( namespace ).toBeDefined();
     } );
 
-    it( "is not an empty object.", function () {
+    it( "should no longer be set to its initial value.", function () {
         expect( namespace ).toNotBe( {} );
     } );
 
-    it( "contains a \"book\" member of type \"function\".", function () {
+    it( "should contain a \"book\" member of type \"function\".", function () {
         expect( typeof( namespace.book ) ).toBe( "function" );
     } );
 
-    it( "contains a \"query\" member of type \"function\".", function () {
+    it( "should contain a \"query\" member of type \"function\".", function () {
         expect( typeof( namespace.query ) ).toBe( "function" );
     } );
 
-    it( "contains a \"data\" member of type \"object\".", function () {
+    it( "should contain a \"data\" member of type \"object\".", function () {
         expect( typeof( namespace.data ) ).toBe( "object" );
     } );
 
@@ -24,19 +24,19 @@ describe( "Namespace", function () {
 
 describe( "Book function", function () {
 
-    it( "returns an object with the expected title \"A Practitioner's Guide to Software Test Design\".", function () {
+    it( "should return an object with the expected title \"A Practitioner's Guide to Software Test Design\".", function () {
         expect( namespace.book( { title: "A Practitioner's Guide to Software Test Design" } ).title ).toBe( "A Practitioner's Guide to Software Test Design" );
     } );
 
-    it( "returns at least one error when it isn't supplied with any parameter.", function () {
+    it( "should return at least one error when it isn't supplied with any parameter.", function () {
         expect( namespace.book().errors.length ).toBeGreaterThan( 0 );
     } );
 
-    it( "returns at least one error when it isn't supplied with at least one valid parameter.", function () {
+    it( "should return at least one error when it isn't supplied with at least one valid parameter.", function () {
         expect( namespace.book( {} ).errors.length ).toBeGreaterThan( 0 );
     } );
 
-    it( "returns at least one error when it is supplied with an unexpected parameter but no valid parameter.", function () {
+    it( "should return at least one error when it is supplied with an unexpected parameter but no valid parameter.", function () {
         expect( namespace.book( { unexpectedParameter: "Unexpected value." } ).errors.length ).toBeGreaterThan( 0 );
     } );
 
@@ -44,27 +44,27 @@ describe( "Book function", function () {
 
 describe( "Data query", function () {
 
-    it( "can find at least one book by the title \"Les Hauts de Hurle-Vent\".", function () {
+    it( "should find at least one book by the title \"Les Hauts de Hurle-Vent\".", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Les Hauts de Hurle-Vent" } ).results.length ).toBeGreaterThan( 0 );
     } );
 
-    it( "can return something for an existing book found by its title.", function () {
+    it( "should return something for an existing book found by its title.", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Wuthering Heights" } ).first() ).toBeTruthy();
     } );
 
-    it( "can read title of the returned book first occurence.", function () {
+    it( "should be able to read the title of a returned book first occurence.", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Wuthering Heights" } ).first().title ).toBe( "Wuthering Heights" );
     } );
 
-    it( "does not find a book that doesn't exist.", function () {
+    it( "should not find a book that doesn't exist.", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "title", filteredValue: "Book title that will not be found" } ).first() ).toBe( undefined );
     } );
 
-    it( "finds a book and reads its title by filtering with its ISBN 10.", function () {
+    it( "should find a book and reads its title by filtering with its ISBN 10.", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "isbn10", filteredValue: "1853260010" } ).first().title ).toBe( "Wuthering Heights" );
     } );
 
-    it( "returns at least one book written in British English.", function () {
+    it( "should return at least one book written in British English.", function () {
         expect( namespace.query( { collection: namespace.data.books, filterBy: "cultureCode", filteredValue: "en-GB" } ).results.length ).toBeGreaterThan( 0 );
     } );
 
--- src/book.js
+++ src/book.js
@@ -1,76 +1,73 @@
 ﻿/*jslint browser: true */
 /*jslint todo: true */
-var namespace = {}; // Replace the name "namespace" with your own namespace, i.e. domain name of your company for example, because it's unlikely to clash with someone else's library.
+var namespace = {};
 
-// Class library.
-namespace.book = function ( properties ) {
+namespace.error = function (properties) {
     "use strict";
-    var that = { errors: [] };
+    var that = { errors: [] }, getMessage;
 
-    if ( !properties ) {
-        that.errors.push( namespace.error( { code: 1 } ) );
+    if (!properties) {
+        that.errors.push(namespace.error({ code: 1 }));
         return that;
     }
 
-    if ( !properties.title && !properties.isbn10 && !properties.isbn13 && !properties.cultureCode ) {
-        that.errors.push( namespace.error( { code: 3 } ) );
+    that.code = properties.code || 2;
+
+    getMessage = function (code) {
+        var error = namespace.query({ collection: namespace.data.errors, filterBy: "code", filteredValue: code }).first();
+
+        return error ? error.message : namespace.query({ collection: namespace.data.errors, filterBy: "code", filteredValue: 6 }).first().message;
+    };
+
+    that.message = getMessage(that.code);
+
+    return that;
+};
+
+namespace.book = function (properties) {
+    "use strict";
+    var that = { errors: [] };
+
+    if (!properties) {
+        that.errors.push(namespace.error({ code: 1 }));
+        return that;
     }
 
-    if ( properties.title ) {
+    if (!properties.title && !properties.isbn10 && !properties.isbn13 && !properties.cultureCode) {
+        that.errors.push(namespace.error({ code: 3 }));
+    }
+
+    if (properties.title) {
         that.title = properties.title;
     }
 
-    if ( properties.isbn10 ) {
+    if (properties.isbn10) {
         that.isbn10 = properties.isbn10;
     }
 
-    if ( properties.isbn13 ) {
+    if (properties.isbn13) {
         that.isbn13 = properties.isbn13;
     }
 
-    if ( properties.cultureCode ) {
+    if (properties.cultureCode) {
         that.cultureCode = properties.cultureCode;
     }
 
     return that;
 };
 
-namespace.error = function ( properties ) {
+namespace.query = function (properties) {
     "use strict";
-    var that = { errors: [] }, getMessage;
-
-    if ( !properties ) {
-        that.errors.push( namespace.error( { code: 1 } ) );
-        return that;
-    }
-
-    that.code = ( properties.code ) ? properties.code : 2;
-
-    getMessage = function ( code ) {
-        if ( code === 1 ) { return "No properties."; }
-        if ( code === 2 ) { return "Missing error code."; }
-        if ( code === 3 ) { return "At least one property member is required."; }
-
-        return "Unexpected error code.";
-    };
-
-    that.message = getMessage( that.code );
-
-    return that;
-};
-
-namespace.query = function ( properties ) {
-    "use strict";
-    var that = {}, findResults;
+    var that = { errors: [] }, findResults;
 
     that.results = [];
 
-    findResults = function ( list, filterBy, filteredValue ) {
+    findResults = function (list, filterBy, filteredValue) {
         var results = [], i;
 
-        for ( i = 0; i < list.length; i += 1 ) {
-            if ( list[ i ] && list[ i ][ filterBy ] === filteredValue ) {
-                results.push( list[ i ] );
+        for (i = 0; i < list.length; i += 1) {
+            if (list[i] && list[i][filterBy] === filteredValue) {
+                results.push(list[i]);
             }
         }
 
@@ -80,71 +77,39 @@ namespace.query = function ( properties ) {
     that.first = function () {
         var result, i;
 
-        // todo: first() has a dependency on findResults() - is that good quality code? Should it be possible for first() to be called by itself?
-        if ( !that.results || that.results.length === 0 ) {
-            namespace.log( { messages: [ { message: "No data can be found.", messageType: "error" } ] } );
-            return undefined;
-        }
-
-        for ( i = 0; i < that.results.length; i += 1 ) {
-            if ( that.results[ i ] ) {
-                result = that.results[ i ];
+        if (!that.results || that.results.length === 0) {
+            that.errors.push(namespace.error({ code: 4 }));
+        } else {
+            for (i = 0; i < that.results.length; i += 1) {
+                if (that.results[i]) {
+                    result = that.results[i];
+                }
             }
         }
 
         return result;
     };
 
-    if ( properties.collection && properties.filterBy && properties.filteredValue ) {
-        that.results = findResults( properties.collection, properties.filterBy, properties.filteredValue );
+    if (properties.collection && properties.filterBy && properties.filteredValue) {
+        that.results = findResults(properties.collection, properties.filterBy, properties.filteredValue);
     } else {
-        namespace.log( { messages: [ { message: "Missing one or several of these properties: list, filterBy and filteredValue.", messageType: "error" } ] } );
+        that.errors.push(namespace.error({ code: 5 }));
     }
 
     return that;
 };
 
-// Data layer.
 namespace.data = {
     books: [
-        namespace.book( { title: "Wuthering Heights", isbn10: "1853260010", isbn13: "978-1853260018", cultureCode: "en-GB" } ),
-        namespace.book( { title: "Les Hauts de Hurle-Vent", isbn10: "2211204252", isbn13: "978-2211204255", cultureCode: "fr-FR" } )
+        namespace.book({ title: "Wuthering Heights", isbn10: "1853260010", isbn13: "978-1853260018", cultureCode: "en-GB" }),
+        namespace.book({ title: "Les Hauts de Hurle-Vent", isbn10: "2211204252", isbn13: "978-2211204255", cultureCode: "fr-FR" })
+    ],
+    errors: [
+        { code: 1, message: "No properties." },
+        { code: 2, message: "Missing error code." },
+        { code: 3, message: "At least one property member is required." },
+        { code: 4, message: "No data can be found." },
+        { code: 5, message: "A required property member is missing." },
+        { code: 6, message: "Unexpected error code." }
     ]
-};
-
-// Presentation layer.
-// todo: consider logging to an ajax call and a database instead of outputting to the client console.
-namespace.log = function ( properties ) {
-    "use strict";
-    var that = {}, write;
-
-    write = function ( message, messageType ) {
-
-        if ( !message ) {
-            message = "Message to log is missing.";
-            messageType = "error";
-        }
-
-        if ( !messageType ) {
-            messageType = "info"; // Default message type.
-        }
-
-        if ( window.console ) {
-            window.console.log( messageType + ": " + message );
-        } else {
-            window.document.writeln( "<p class=" + messageType + ">" + messageType + ": " + message + "</p>" );
-        }
-    };
-
-    if ( !properties || !properties.messages ) {
-        write( "Constructor has no messages to log.", "error" );
-    }
-
-    if ( properties && properties.messages ) {
-        properties.messages.forEach( function ( messageObject ) {
-            write( messageObject.message, messageObject.messageType );
-        } );
-    }
-
-    return that;
 };
